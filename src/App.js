7f2eb8c01a02ad7ff2e590d6ce3bf5def1957158
Tóth <tothbalint719@gmail.com>
2021-02-09 12:03:39 +0100

    day 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
+const initialState = [
+  {
+    id: 1,
+    title: "Lajos bevásárlólistája",
+    todos: [
+      { id: 2, title: "Aldi", description: "Tej, kenyér"},
+      { id: 3, title: "Lidl", description: "Hús"},
+    ]
+  },
+  {
+    id: 4,
+    title: "Béla bevásárlólistája",
+    todos: [
+      { id: 5, title: "CBA", description: "Alma, Banán"},
+    ]
+  },
+]
+
 function App() {
+
+  const [ dashboards, setDashboards ] = useState(initialState)
+  const [ dashboardInput, setDashboardInput ] = useState("")
+
+  const deleteDashboard = (id) => {
+    const newDashboards = dashboards.filter(dashboard => dashboard.id !== id)
+    setDashboards(newDashboards)
+  }
+
+  const createDashboard = () => {
+    const newDashboards = [ ...dashboards, {
+      id: new Date().getTime(),
+      title: dashboardInput,
+      todos: []
+    } ]
+    setDashboards(newDashboards)
+    setDashboardInput("")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <input value={dashboardInput} onChange={(e) => setDashboardInput(e.target.value)} />
+        <button onClick={() => createDashboard()}>Create dashboard</button>
+      </nav>
+      <main>
+        { dashboards.map(dashboard =>
+          <section key={dashboard.id}>
+            <h2>{ dashboard.title }</h2>
+            { dashboard.todos.map(todo =>
+              <article key={todo.id}>
+                <h3>{ todo.title }</h3>
+                <p>{ todo.description }</p>
+              </article>
+            ) }
+            <button onClick={() => deleteDashboard(dashboard.id)}>Delete</button>
+          </section>
+        ) }
+      </main>
     </div>
   );
 }
