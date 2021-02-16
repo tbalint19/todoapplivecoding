642c7971a76148e493e51e744caeef35f3d09463
Tóth <tothbalint719@gmail.com>
2021-02-16 12:24:06 +0100

    initial tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,45 @@
-import { withNewDashboard } from './producers/withNewDashboard'
+import { withNewTodo } from './producers/withNewTodo'
 
-test('renders learn react link', () => {
-  const base = [
-    { id: 1, title: "1d", todos: [ { id: 1, title: "1t", description: "1d"}]}
+test('Adds todo to correct dashboard', () => {
+
+  // given
+  const baseState = [
+    {
+      id: 1,
+      title: "first dashboard",
+      todos: [
+        { id: 1, title: "first title", description: "first desc"}
+      ]
+    }
   ]
-  const nextState = withNewDashboard(base)
-  expect(next).toBe();
-});
+
+  const spy = jest
+    .spyOn(global, 'Date')
+    .mockImplementation(() => ({ getTime: () => 2 }))
+
+  // when
+  const nextState = withNewTodo(baseState, 1, "second title", "second desc")
+
+  spy.mockRestore()
+
+  // then
+  expect(nextState).toEqual([
+    {
+      id: 1,
+      title: "first dashboard",
+      todos: [
+        { id: 1, title: "first title", description: "first desc"},
+        { id: 2, title: "second title", description: "second desc"},
+      ]
+    }
+  ])
+  expect(baseState).toEqual([
+    {
+      id: 1,
+      title: "first dashboard",
+      todos: [
+        { id: 1, title: "first title", description: "first desc"}
+      ]
+    }
+  ])
+})
